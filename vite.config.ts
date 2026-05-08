89690e655795ec1c842283ea60fa1a8e35b7b2e1
vishalraccha143 <vishalraccha143@gmail.com>
2026-05-08 14:41:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: {
+      origin: ['http://45.64.199.162', 'http://182.48.233.226'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
